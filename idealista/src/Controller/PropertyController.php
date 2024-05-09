116b26b8ff2f002f9604016ccb17ec04be5a9ff9
Alex Madrazo <alex.madrazo@bgamestudios.com>
2024-05-09 10:49:59 +0200

    added property controller api method

--- idealista/src/Controller/PropertyController.php
+++ idealista/src/Controller/PropertyController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+
 
 
 class PropertyController extends AbstractController
@@ -20,6 +22,12 @@ class PropertyController extends AbstractController
 
     #[Route('api/properties', name: 'api_properties')]
     public function getProperties(Request $request): Response
+    {
+        return $this->json(true);
+    }
+
+    #[Route('api/property', name: 'create_property', methods: ['POST'])]
+    public function createProperty(Request $request, EntityManagerInterface $entityManager): Response
     {
         return $this->json(true);
     }
